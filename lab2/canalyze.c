8d47e4f59ede82e4ca00313d4ed57dee9d3e87fd
berda <bardit@hotmail.com>
2011-11-15 16:30:55 +0200

    first commit

--- lab2/canalyze.c
+++ lab2/canalyze.c
@@ -7,42 +7,20 @@
 #include <netinet/in.h>
 #define NAMELEN 64
 
-int str_alp(char *s1,char *s2) {
-  //  if(strcmp(s1,s2)==0){
-     // return(0);}   //if they are equal
-	int i;
-        for(i=0;s1[i]!=0;i++) {
-              if(s1[i] > s2[i]) { 
-		return(1);
-		
-	      }
-              else return(2);
-        }
- 
-   // return (2);  //hey if they are not equal and s1 not greater than s2 then s2 is greater
+str_alp(const void *p1, const void *p2)
+{
+   /* The actual arguments to this function are "pointers to
+      pointers to char", but strcmp(3) arguments are "pointers
+      to char", hence the following cast plus dereference */
+	const struct namestat *n1;
+	const struct namestat *n2;
+	n1 = (struct namestat*)p1;
+	n2 = (struct namestat*)p2;
+	const char *s1 = n1->name;
+	const char *s2 = n2->name;
+   return strcmp(s1, s2);
 }
 
-namelist sort_list(namelist nl){
-  int i;
-  int j;
-  int ans;
- namelist orderlist =nl;
-  char *c;
-  for(i = 0; i!=orderlist->size; ++i){
-      for(j=1;j!=orderlist->size;++j){
-	ans=str_alp(orderlist->names[i].name, orderlist->names[j].name);
-	if(ans==1){
-	  namelist n2 = make_namelist();
-	  printf("%d", ans);
-	  n2->names[0] = orderlist->names[j];
-	   printf("%s", n2->names[0].name);
-	  nl->names[j]=nl->names[i];
-	  nl->names[i]=n2->names[0];
-     }
-   } 
-}
-return nl;
-}
 
 int main(int argc, char *argv[])  {
   
@@ -63,38 +41,11 @@ int main(int argc, char *argv[])  {
     fclose(stream);
 
   }
-    /*
-    int counter=0;
- // for(index=1; index<argc, index++){  
-    
- 	//while(fgetname(name, sizeof(name), f1)){
-//	printf("%s\n", name);
-
-     while (!feof(f1)) {
-	  char word[100];
-	  char c=fgetc(f1);
-	if((!isdigit(c))&&(!isalpha(c))) { 
-	    word[counter] = 0;
-	    add_name(nl,word);
-	    counter=0;
+  qsort(nl->names,nl->size, sizeof(nl->names[0]), str_alp);
+int j;
+	for(j = 0; j!=nl->size; ++j) {
+		printf("%s %d\n",nl->names[j].name,nl->names[j].count);
 	}
-     else{
-    word[counter]=c;
-    counter++;
-  }   
-      }
-
- */
-namelist orderlist = make_namelist();
-orderlist= sort_list(nl);
-
- int i;
-for(i = 0; i!=orderlist->size; ++i){
-  printf("%s", orderlist->names[i].name);
-   printf("%d", orderlist->names[i].count);
-   printf("\n");
-
- }
  
 return 0;
 }
