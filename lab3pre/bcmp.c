9acde1594ec6cc68dd4f461146f25245a755e674
berda <bardit@hotmail.com>
2011-11-26 15:21:39 +0200

    first commit

--- lab3pre/bcmp.c
+++ lab3pre/bcmp.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <ctype.h>
 #include <assert.h>
 
 int main(int argc, char *argv[]){
 
   int counter =0;
-      FILE *f1= fopen(argv[1], "rb" ); 
-      FILE *f2= fopen(argv[2], "rb" ); 
+      FILE *f1= fopen(argv[1], "rb+" ); 
+      FILE *f2= fopen(argv[2], "rb+" ); 
       	if (!f1)
 	{
-		fprintf(stderr, "Unable to open file %s", argv[1]);
-		return 0;
+	  fprintf(stderr, "Unable to open file %s", argv[1]);
+	  return 0;
 	}
 	     	if (!f2)
 	{
 		fprintf(stderr, "Unable to open file %s", argv[1]);
 		return 0;
 	}
-    while( (!feof(f1))){
-   // char c1[100];
-   // char c2[100];
+    while( (!feof(f1))&&(!feof(f2))){
     int c1=fgetc(f1);
     int c2=fgetc(f2);
     counter++;
     if(c1!=c2){
-     printf("%d", counter);     
-     printf("\n");
+     printf("byte \%d", counter);     
+     printf(" ");
     printf("%d", c1);
-    printf("\n");
+    printf(" ");
      printf("%d", c2);
-    
-      break;
+    printf("\n");
+   //   break;
     }
-    
-        //fread(&my_record,sizeof(struct rec),1,f);
-	//    printf("%d\n",my_record.x);
-    }
+ }
 
 return 0;
 
